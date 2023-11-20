78254bd17e2b564f4a746352361b4ac2e8b1e8af
Pradeepta Sahu <psahu@nasuni.com>
2023-11-20 22:54:33 +0530

    Export Only V.1

--- main.tf
+++ main.tf
@@ -112,6 +112,7 @@ data "local_file" "aws_conf_secret_key" {
       "sudo apt install terraform",
       "terraform -v",
       "which terraform",
+      "echo @@@@@@@@@@@ terraform Installed @@@@@@@@@",
       "sudo apt install jq -y",
       "sudo apt install zip -y",
       "sudo apt install unzip -y",
